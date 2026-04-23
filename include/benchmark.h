84b690fdf75b38ff35bdcfa3b0be4de71140213d
Zetsyog <crossetti@unistra.fr>
2026-04-23 14:51:08 +0200

    📝 Update benchmark.h for C23 compatibility and remove C23 standard flag from Makefile

--- include/benchmark.h
+++ include/benchmark.h
@@ -1,6 +1,17 @@
 #ifndef BENCHMARK_H
 #define BENCHMARK_H
 
+/* C2x/C23 or later? */
+#if (defined(__STDC__) && defined(__STDC_VERSION__) &&                         \
+	 (__STDC_VERSION__ >= 202300L))
+#include <stddef.h> /* nullptr_t */
+/* pre C23, pre C++11 or non-standard */
+#else
+#ifndef nullptr
+#define nullptr NULL
+#endif // nullptr
+#endif // __STDC_VERSION__
+
 #include <benchmark/arrays.h>
 #include <benchmark/dump.h>
 #include <benchmark/time.h>
